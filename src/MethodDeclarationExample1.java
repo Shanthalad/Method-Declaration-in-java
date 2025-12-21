c89173372ed61a8d252c1d7d17b52b5a852f2f29
Lenovo <Lenovo@DESKTOP-PNAR7AQ>
2025-12-21 21:18:59 +0530

    An example of Method Declaration in java

---
+++ src/MethodDeclarationExample1.java
@@ -0,0 +1,21 @@
+
+public class MethodDeclarationExample1 {
+		public String name() {
+			
+			return "hello";
+			
+		}
+		
+		public void welcome( char c, double d) {
+			
+			
+		}
+		
+		public double bye(float f, long l, double d) {
+			
+			return d;
+			
+			
+	}
+}
+
